6fcd914163d78d39218f757ee51ba43ad5856a3a
Topseen-a <abodunrintopseen@gmail.com>
2025-11-29 21:14:45 +0100

    morning snacks

---
+++ morningsnacks/LeapYear.java
@@ -0,0 +1,10 @@
+public class LeapYear{
+    public static void main(String[] args){
+
+    for (int year = 1900; year <= 2025; year++){
+        if ((year % 4 == 0 && year %  100 != 0) || (year % 400 == 0)){
+            System.out.println(year);
+            }
+    }
+    }
+}
