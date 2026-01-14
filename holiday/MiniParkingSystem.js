4e3858dfb5e82b949ac30eb3dbbd7200b82eb3ff
Topseen-a <abodunrintopseen@gmail.com>
2026-01-14 21:42:47 +0100

    holiday

--- holiday/MiniParkingSystem.js
+++ holiday/MiniParkingSystem.js
@@ -19,68 +19,80 @@ while (true){
 
     switch (choice){
         case 1:
-            let index;
-            for (index = 0; index < parkingSlot.length; index++){
-                if (parkingSlot[index] == 0){
-                    parkingSlot[index] = 1;
-                    console.log("Your car is parked at slot " + (index + 1));
-                    break;
-                }
-            }
-
-            if (index == parkingSlot.length){
-                System.out.println("Parking lot is full");
-            }
-            console.log();
+            parkCarAutomatically();
             break;
-
         case 2:
-            let slotChoice = parseInt(prompt("Enter a slot to park from (1-20): "));
-
-            if (slotChoice < 1 || slotChoice > 20){
-                console.log("Invalid choice");
-            } 
-            else if (parkingSlot[slotChoice - 1] == 1){
-                console.log("Slot already occupied");
-            } 
-            else {
-                parkingSlot[slotChoice - 1] = 1;
-                console.log("Your car is parked at slot " + slotChoice + "\n");
-            }
+            chooseSlot();
             break;
-
         case 3:
-            let removeSlotNumber = parseInt(prompt("Enter the slot number to remove your car: "));
-
-            if (removeSlotNumber < 1 || removeSlotNumber > 20){
-                console.log("Invalid choice");
-            } 
-            else if (parkingSlot[removeSlotNumber - 1] == 0){
-                console.log("Slot already empty");
-            } 
-            else {
-                parkingSlot[removeSlotNumber - 1] = 0;
-                console.log("Your car is removed from slot " + removeSlotNumber + "\n");
-            }
+            removeCar();
             break;
-
         case 4:
-            console.log("\nParking status:");
-            for (let count = 0; count < parkingSlot.length; count++) {
-                let status = "";
-                if (parkingSlot[count] == 0) {
-                    status = "Empty";
-                } 
-                else {
-                    status = "Occupied";
-                }
-                console.log("Slot " + (count + 1) + ": " + status);
-            }
-            console.log();
+            displayParkingStatus();
             break;
-
         default:
             console.log("Invalid choice, choose an option from the listed options\n");
             break;
     }
+}
+
+function parkCarAutomatically(){
+    let index;
+    for (index = 0; index < parkingSlot.length; index++){
+        if (parkingSlot[index] == 0){
+            parkingSlot[index] = 1;
+            console.log("Your car is parked at slot " + (index + 1));
+            break;
+        }
+    }
+
+    if (index == parkingSlot.length){
+        console.log("Parking lot is full");
+    }
+    console.log();
+}
+
+function chooseSlot(){
+    let slotChoice = parseInt(prompt("Enter a slot to park from (1-20): "));
+
+    if (slotChoice < 1 || slotChoice > 20){
+        console.log("Invalid choice");
+    } 
+    else if (parkingSlot[slotChoice - 1] == 1){
+        console.log("Slot already occupied");
+    } 
+    else {
+        parkingSlot[slotChoice - 1] = 1;
+        console.log("Your car is parked at slot " + slotChoice + "\n");
+    }
+}
+
+function removeCar(){
+    let removeSlotNumber = parseInt(prompt("Enter the slot number to remove your car: "));
+
+    if (removeSlotNumber < 1 || removeSlotNumber > 20){
+        console.log("Invalid choice");
+    } 
+    else if (parkingSlot[removeSlotNumber - 1] == 0){
+        console.log("Slot already empty");
+    } 
+    else {
+        parkingSlot[removeSlotNumber - 1] = 0;
+        console.log("Your car is removed from slot " + removeSlotNumber + "\n");
+    }
+}
+
+function displayParkingStatus(){
+    console.log("\nParking status:");
+    for (let count = 0; count < parkingSlot.length; count++){
+        let status = "";
+        if (parkingSlot[count] == 0) {
+            status = "Empty";
+        } 
+        else {
+            status = "Occupied";
+        }
+        console.log("Slot " + (count + 1) + ": " + status);
+    }
+    console.log();
 }
